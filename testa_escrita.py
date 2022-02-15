646e2e8381e079f5fb117130a6b9b7b8af72075c
JuhJSimoes <julianajsimoes@gmail.com>
2022-02-15 05:03:05 -0300

    Trabalhando_I-O_150222

--- testa_escrita.py
+++ testa_escrita.py
@@ -1,3 +1,23 @@
-arquivos_contatos = open('dados/contatos_escrita.csv', encoding= 'latin_1', mode = 'a')
-contato = '11,Carol,carol@carol.com.br\n'
-arquivos_contatos.write(contato)
+
+arquivos_contatos = open('dados/contatos_escrita.csv', encoding= 'latin_1', mode = 'a+')
+
+contatos = ['11,Carol,carol@carol.com.br\n'
+           '12,Ana,ana@ana.com.br\n'
+           '13,Thais,tais@tais.com.br\n'
+           '14,Felipe,felipe@felipe.com.br\n'
+           ]
+
+for contato in contatos:
+    arquivos_contatos.write(contato)
+    
+arquivos_contatos.flush()
+arquivos_contatos.seek(28)
+arquivos_contatos.write('12,Ana,ana@ana.com.br\n'.upper())
+arquivos_contatos.flush()
+arquivos_contatos.seek(0)
+
+for linha in arquivos_contatos:
+    print(linha)
+    
+    
+arquivos_contatos.close()
